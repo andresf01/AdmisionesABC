cb11f249db769836290d35ea0d4ac869239ef7b7
Andresf01 <andres.felipe.martinez@correounivalle.edu.co>
2016-10-25 19:05:51 +0000

    Red line

--- static/assets/js/base.js
+++ static/assets/js/base.js
@@ -1,16 +1,26 @@
 $('document').ready(function(){
     
-    // Set red line 
-    var myLocation = window.location.href;
-    console.log('location: '+myLocation);
-    var directories = myLocation.split("/");
-    var page = directories[directories.length - 2];
+    // Set red line script
+    var directories = window.location.href.split("/");
+    var page = directories[directories.length - 1];
+    var i = 2;
+    
+    // While for find the app
+    while( page == '' || page == '/')
+        page = directories[directories.length - (i++)];
+    
+    // Message to confirm location   
     console.log('located in: '+page);
-    $('#b_'+page).addClass('active');
+    
+    // If my app is home
     if (page == 'admisionesabc-andresf01.c9users.io')
     {
         $('#b_').addClass('active');
     }
-    //admisionesabc-andresf01.c9users.io
+    // If the app is another
+    else
+    {
+        $('#b_'+page).addClass('active');
+    }
     
 });
